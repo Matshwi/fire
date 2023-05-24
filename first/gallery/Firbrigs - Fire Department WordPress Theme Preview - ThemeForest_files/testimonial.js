296835ec5921d3830e6d8631e4a0e34fa675f3b5
Matshwi <51079456+Matshwi@users.noreply.github.com>
2023-05-24 15:08:12 +0200

    Add files via upload

---
+++ first/gallery/Firbrigs - Fire Department WordPress Theme Preview - ThemeForest_files/testimonial.js
@@ -0,0 +1,2 @@
+(function($){"use strict";var three_item_carosel=function($scope,$){if($('.three-item-carousel').length){$('.three-item-carousel').owlCarousel({loop:true,margin:30,nav:true,smartSpeed:1000,autoplay:500,navText:['<span class="flaticon-left-2"></span>','<span class="flaticon-right-1"></span>'],responsive:{0:{items:1},480:{items:1},600:{items:2},800:{items:2},1024:{items:3}}});}}
+$(window).on('elementor/frontend/init',function(){elementorFrontend.hooks.addAction('frontend/element_ready/firbrigs_testimonial.default',three_item_carosel);});})(window.jQuery);
